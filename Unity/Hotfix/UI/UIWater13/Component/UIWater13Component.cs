a9b096e790e4fc0ffbf1af2b54423bc41ea326d6
shiliupingGithub <582441842@qq.com>
2017-12-27 17:59:57 +0800

    asdasd

---
+++ Unity/Hotfix/UI/UIWater13/Component/UIWater13Component.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+namespace Hotfix
+{
+    [ObjectEvent]
+    public class UIWater13ComponentEvent : ObjectEvent<UIWater13Component>, IAwake
+    {
+        public void Awake()
+        {
+            this.Get().Awake();
+        }
+    }
+
+    public class UIWater13Component : Component
+    {
+        public void Awake()
+        {
+
+        }
+    }
+}
